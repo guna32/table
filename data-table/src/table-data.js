60661d4f8c878906cec68f35673423fbce706ff4
prasad hegde <gunathirtytwo@gmail.com>
2020-01-23 17:34:35 +0530

    sample table data added

---
+++ data-table/src/table-data.js
@@ -0,0 +1,18 @@
+export const countryDetails = {
+  data: [
+    {
+      country: "India",
+      capital: "New Delhi",
+      monetaryUnit: "Rupee",
+      literacyRate: "62.8",
+      government: "Federal republic"
+    },
+    {
+      country: "Finland",
+      capital: "Helsinki",
+      monetaryUnit: "Euro",
+      literacyRate: "100",
+      government: "Republic"
+    }
+  ]
+};
