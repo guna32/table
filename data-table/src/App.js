6f30920f5a4639176f7daf23eadc136086f3884f
prasad hegde <gunathirtytwo@gmail.com>
2020-01-23 17:39:18 +0530

    -m

--- data-table/src/App.js
+++ data-table/src/App.js
@@ -4,8 +4,12 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <div style={{ background: "gray", width: "30%" }}>form here</div>
-      <div style={{ background: "lightgray", width: "70%" }}>table here</div>
+      <div style={{ background: "gray", width: "30%" }}>
+        <h1>Insert Form</h1>
+      </div>
+      <div style={{ background: "lightgray", width: "70%" }}>
+        <h1>Data table</h1>
+      </div>
     </div>
   );
 }
