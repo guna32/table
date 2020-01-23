b893e9b71dbf5c0899d698fc6a320ddf1a72a57f
prasad hegde <gunathirtytwo@gmail.com>
2020-01-23 21:27:53 +0530

    updated table 1.0

--- data-table/src/components/data-table/index.js
+++ data-table/src/components/data-table/index.js
@@ -1,14 +1,35 @@
 import React, { useState } from "react";
+import { TableCell, Title, TW } from "./style";
 
 const Table = ({ data, title }) => {
+  return (
+    <TW>
+      <Title>{title}</Title>
+      <RawTable data={data}></RawTable>
+    </TW>
+  );
+};
+
+const RawTable = ({ data }) => {
   const tHeaders = Object.keys(data[0]);
   return (
     <table>
       <tr>
-        {tHeaders.map(item => (
-          <td>{item}</td>
+        {tHeaders.map(th => (
+          <td>
+            <TableCell cellType="header">{th}</TableCell>
+          </td>
         ))}
       </tr>
+      {data.map(tr => (
+        <tr key={JSON.stringify(tr)}>
+          {tHeaders.map(th => (
+            <td>
+              <TableCell key={JSON.stringify(th)}>{tr[th]}</TableCell>
+            </td>
+          ))}
+        </tr>
+      ))}
     </table>
   );
 };
